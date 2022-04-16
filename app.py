afacc42fb5d783646fbf7a7d4080ec548360eb78
VishDroid-dev <vk2345732@gmail.com>
2022-04-16 21:44:39 +0530

    dev

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def callback():
 
     resp, content = client.request(access_token_url, "POST")
     access_token = dict(urllib.parse.parse_qsl(content))
-
+    print(access_token)
     screen_name = access_token[b'screen_name'].decode('utf-8')
     user_id = access_token[b'user_id'].decode('utf-8')
 
